bb8ec1ffe55a19f4830e22d720afcf88a89bb10d
slear08 <slearmendoza8@gmail.com>
2025-04-01 07:17:07 +0800

    added clear session and cookie

--- src/api/global/logout.mutation.ts
+++ src/api/global/logout.mutation.ts
@@ -16,6 +16,12 @@ export const useLogout = () => {
         onSuccess: () => {
             localStorage.removeItem('user');
             localStorage.removeItem('auth');
+            sessionStorage.clear();
+
+            document.cookie.split(';').forEach((cookie) => {
+                const [name] = cookie.split('=');
+                document.cookie = `${name.trim()}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+            });
         },
     });
 };
